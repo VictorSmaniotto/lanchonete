794dc76fe3e60e183ff738de95d2035985babb01
Victor Smaniotto <victorsmaniotto@hotmail.com>
2023-04-10 20:29:58 -0300

    finalização do overlay no menu

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 const btnMenu = document.getElementById("btn-menu");
 const btnClose = document.getElementById("btn-close");
 const menu = document.getElementById("menu-principal");
+const overlay = document.getElementById("overlay");
 
 btnMenu.addEventListener("click", function (e) {
   e.preventDefault(); //cancela outros eventos de click
   menu.classList.toggle("menu-show");
+  overlay.classList.toggle("overlay-show");
 });
 
 btnClose.addEventListener("click", function (e) {
   e.preventDefault();
   menu.classList.toggle("menu-show");
+  overlay.classList.toggle("overlay-show");
+});
+
+overlay.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (event.target === this) {
+    overlay.classList.remove("overlay-show");
+    menu.classList.toggle("menu-show");
+  }
 });
